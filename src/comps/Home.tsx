a4f975ab4164f7a19109c6e8056df5f74134aabe
FarDean <sfh1212@gmail.com>
2021-05-16 22:55:38 +0430

    worked on home page a little

--- src/comps/Home.tsx
+++ src/comps/Home.tsx
@@ -1,3 +1,15 @@
-export const Home: React.FC = () => {
-	return <h1>Home</h1>;
+import styles from "./../styles/Home.module.css";
+
+export const Home = (): JSX.Element => {
+	return (
+		<div className={styles.overlay}>
+			<div className={styles.nav}>
+				<div className={styles.left}>FarDean</div>
+				<div className={styles.right}>
+					<div>Home</div>
+					<div>Works</div>
+				</div>
+			</div>
+		</div>
+	);
 };
