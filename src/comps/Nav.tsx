31672956f84ee7d4ba6e74fa2fcf6d082e537300
FarDean <sfh1212@gmail.com>
2021-05-18 13:37:37 +0430

    changed some css in nav

--- src/comps/Nav.tsx
+++ src/comps/Nav.tsx
@@ -6,23 +6,26 @@ export const Nav = (): JSX.Element => {
 		<div className={styles.nav}>
 			<div className={styles.active}>
 				<Link to="/">
-					<i className="fas fa-laptop-house"></i> Home
+					<i className="fas fa-laptop-house"></i>
+					<div>Home</div>
 				</Link>
 			</div>
 			<div>
 				<Link to="/about">
-					<i className="far fa-address-card"></i> About
+					<i className="far fa-address-card"></i> <div> About</div>
 				</Link>
 			</div>
 			<div>
 				<Link to="/works">
-					<i className="fas fa-scroll"></i> Works
+					<i className="fas fa-scroll"></i>
+					<div>Works</div>
 				</Link>
 			</div>
 
 			<div>
 				<Link to="/contact">
-					<i className="far fa-envelope"></i> Contact Me
+					<i className="far fa-envelope"></i>
+					<div>Contact Me</div>
 				</Link>
 			</div>
 		</div>
