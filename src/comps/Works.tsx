8b4d7c30dcf1bdf4824592e104ad081a57698afe
FarDean <sfh1212@gmail.com>
2021-05-27 21:52:01 +0430

    some work on Works comp

--- src/comps/Works.tsx
+++ src/comps/Works.tsx
@@ -1,3 +1,15 @@
+import styles from "./../styles/Works.module.css";
+
 export const Works = (): JSX.Element => {
-	return <div>works</div>;
+	return (
+		<div>
+			<div className={styles.parent}>
+				<div className={styles.div1}>
+					<div></div>
+				</div>
+				<div className={styles.div2}>Work3</div>
+				<div className={styles.div3}>Work3</div>
+			</div>
+		</div>
+	);
 };
