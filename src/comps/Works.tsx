5a36dc5da2091bcae102cdcc3f1cb60586cd5143
FarDean <sfh1212@gmail.com>
2021-05-28 11:03:09 +0430

    some work on works

--- src/comps/Works.tsx
+++ src/comps/Works.tsx
@@ -5,10 +5,35 @@ export const Works = (): JSX.Element => {
 		<div>
 			<div className={styles.parent}>
 				<div className={styles.div1}>
-					<div></div>
+					<div className={styles.cardcontent}>
+						<h3>MERN Blog</h3>
+						<div>
+							<div>Details</div>
+							<div>github</div>
+							<div>Link</div>
+						</div>
+					</div>
 				</div>
-				<div className={styles.div2}>Work3</div>
-				<div className={styles.div3}>Work3</div>
+				<div className={styles.div2}>
+					<div className={styles.cardcontent}>
+						<h3>MERN Blog</h3>
+						<div>
+							<div>Details</div>
+							<div>github</div>
+							<div>Link</div>
+						</div>
+					</div>
+				</div>
+				<div className={styles.div3}>
+					<div className={styles.cardcontent}>
+						<h3>MERN Blog</h3>
+						<div>
+							<div>Details</div>
+							<div>github</div>
+							<div>Link</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
