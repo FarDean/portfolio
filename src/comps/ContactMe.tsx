730fc9ba830df25713ed615ed32bff83b835266e
FarDean <64462409+FarDean@users.noreply.github.com>
2021-06-15 11:57:40 +0430

    didnt pick up env agin

--- src/comps/ContactMe.tsx
+++ src/comps/ContactMe.tsx
@@ -60,83 +60,93 @@ export const ContactMe: React.FC = (): JSX.Element => {
 			);
 	};
 
-	if (!process.env.REACT_APP_RECAPTCHA_SITE_KEY) return <Spinner />;
+	console.log(process.env.REACT_APP_SITE_KEY);
 
-	return loading ? (
-		<Spinner />
-	) : (
-		<div className={styles.container}>
-			<div className={styles.hr}>
-				<div></div>
-				<h3>Contact Me</h3>
-				<div></div>
-			</div>
-			<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-				<div>
-					<label htmlFor="name">Name</label>
+	if (process.env.REACT_APP_SITE_KEY)
+		return loading ? (
+			<Spinner />
+		) : (
+			<div className={styles.container}>
+				<div className={styles.hr}>
+					<div></div>
+					<h3>Contact Me</h3>
+					<div></div>
 				</div>
-				<div>
-					<input
-						{...register("name", { required: true, minLength: "3", maxLength: "30" })}
-						type="text"
-						name="name"
-						id="name"
-					/>
-
-					{errors.name && <h6>Name must be at least 3 and maximum 30 charachters!</h6>}
-				</div>
-				<div>
-					<label htmlFor="email">Email</label>
-				</div>
-				<div>
-					<input
-						{...register("email", {
-							required: true,
-							pattern: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
-						})}
-						type="email"
-						name="email"
-					/>
-					{errors.email?.type === "required" && <h6>You need to fill in your Email!</h6>}
-					{errors.email?.type === "pattern" && <h6>Please use a valid Email!</h6>}
-				</div>
-				<div>
-					<label htmlFor="message">Message:</label>
-				</div>
-				<div>
-					<textarea
-						{...register("message", {
-							required: true,
-							minLength: "20",
-							maxLength: "300",
-						})}
-						spellCheck
-						name="message"
-						id=""
-						cols={30}
-						rows={10}
-					></textarea>
-					{errors.message && (
-						<h6>Your message should be at least 20 and maximum 300 charachters!</h6>
-					)}
-				</div>
-
-				<Controller
-					control={control}
-					name="recaptcha"
-					rules={{ required: true }}
-					render={({ field: { onChange } }) => (
-						<ReCAPTCHA
-							sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY!}
-							onChange={onChange}
+				<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+					<div>
+						<label htmlFor="name">Name</label>
+					</div>
+					<div>
+						<input
+							{...register("name", {
+								required: true,
+								minLength: "3",
+								maxLength: "30",
+							})}
+							type="text"
+							name="name"
+							id="name"
 						/>
-					)}
-				/>
-				{errors.recaptcha && <h6>You need to complete the recaptcha!</h6>}
 
-				<input disabled={!isValid || loading} type="submit" />
-			</form>
-			<Modal open={open} message={message} resolved={resolved} name={watch("name")} />
-		</div>
-	);
+						{errors.name && (
+							<h6>Name must be at least 3 and maximum 30 charachters!</h6>
+						)}
+					</div>
+					<div>
+						<label htmlFor="email">Email</label>
+					</div>
+					<div>
+						<input
+							{...register("email", {
+								required: true,
+								pattern: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+							})}
+							type="email"
+							name="email"
+						/>
+						{errors.email?.type === "required" && (
+							<h6>You need to fill in your Email!</h6>
+						)}
+						{errors.email?.type === "pattern" && <h6>Please use a valid Email!</h6>}
+					</div>
+					<div>
+						<label htmlFor="message">Message:</label>
+					</div>
+					<div>
+						<textarea
+							{...register("message", {
+								required: true,
+								minLength: "20",
+								maxLength: "300",
+							})}
+							spellCheck
+							name="message"
+							id=""
+							cols={30}
+							rows={10}
+						></textarea>
+						{errors.message && (
+							<h6>Your message should be at least 20 and maximum 300 charachters!</h6>
+						)}
+					</div>
+
+					<Controller
+						control={control}
+						name="recaptcha"
+						rules={{ required: true }}
+						render={({ field: { onChange } }) => (
+							<ReCAPTCHA
+								sitekey={process.env.REACT_APP_SITE_KEY!}
+								onChange={onChange}
+							/>
+						)}
+					/>
+					{errors.recaptcha && <h6>You need to complete the recaptcha!</h6>}
+
+					<input disabled={!isValid || loading} type="submit" />
+				</form>
+				<Modal open={open} message={message} resolved={resolved} name={watch("name")} />
+			</div>
+		);
+	return <Spinner />;
 };
