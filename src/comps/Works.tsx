93535246be70b876a58a446ecd004974449d68bc
FarDean <64462409+FarDean@users.noreply.github.com>
2021-06-30 11:26:10 +0430

    projects comp done

--- src/comps/Works.tsx
+++ src/comps/Works.tsx
@@ -1,6 +1,9 @@
 import styles from "./../styles/Works.module.css";
 import football from "./../assets/football480.jpg";
 import blog from "./../assets/mern-blog-480.jpg";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 export const Works = (): JSX.Element => {
 	return (
@@ -14,11 +17,55 @@ export const Works = (): JSX.Element => {
 				<div className={styles.div1}>
 					<div>
 						<img src={blog} loading="lazy" alt="MERN Blog" />
+						<div className={styles.content}>
+							<div>
+								<a
+									aria-label="MERN Blog Github"
+									target="_blank"
+									rel="noreferrer"
+									href="https://github.com/FarDean/MERN-Blog"
+								>
+									<FontAwesomeIcon icon={faGithub} />
+								</a>
+							</div>
+							<div>
+								<a
+									aria-label="MERN Blog Github"
+									target="_blank"
+									rel="noreferrer"
+									href="https://fardeanblog.herokuapp.com/"
+								>
+									<FontAwesomeIcon icon={faLink} />
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div className={styles.div2}>
 					<div>
 						<img src={football} loading="lazy" alt="Football App" />
+						<div className={styles.content}>
+							<div>
+								<a
+									aria-label="Football App Github "
+									target="_blank"
+									rel="noreferrer"
+									href="https://github.com/FarDean/fantastic-fiesta"
+								>
+									<FontAwesomeIcon icon={faGithub} />
+								</a>
+							</div>
+							<div>
+								<a
+									aria-label="MERN Blog Github"
+									target="_blank"
+									rel="noreferrer"
+									href="https://fardean-football-app.netlify.app/"
+								>
+									<FontAwesomeIcon icon={faLink} />
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 				{/* <div className={styles.div3}> project3</div> */}
