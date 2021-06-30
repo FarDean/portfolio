f013002c8cae7253116f78260e0233828e1eef37
FarDean <64462409+FarDean@users.noreply.github.com>
2021-06-30 12:01:48 +0430

    style to about page

--- src/comps/About.tsx
+++ src/comps/About.tsx
@@ -1,16 +1,28 @@
 import portrait from "././../assets/portrait.jpg";
-
+import styles from "./../styles/About.module.css";
 export const About: React.FC = (): JSX.Element => {
 	return (
-		<div>
-			<div>
-				<img src={portrait} alt="Face" />
+		<div className={styles.container}>
+			<div className={styles.hr}>
+				<div></div>
+				<h3>About Me</h3>
+				<div></div>
 			</div>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi repudiandae modi
-				sequi rem eos ipsam mollitia quia. Fuga quae nemo quas laboriosam inventore minima
-				minus natus optio dignissimos autem dolores quibusdam quis blanditiis impedit,
-				distinctio est ad totam debitis voluptates! Deleniti facilis praesentium delectus?
+			<div className={styles.about}>
+				<div className={styles.img}>
+					<img loading="lazy" src={portrait} alt="Face" />
+				</div>
+				<div className={styles.p}>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+						repudiandae modi sequi rem eos ipsam mollitia quia. Fuga quae nemo quas
+						laboriosam inventore minima minus natus optio dignissimos autem dolores
+					</p>
+					<p>
+						quibusdam quis blanditiis impedit, distinctio est ad totam debitis
+						voluptates! Deleniti facilis praesentium delectus?
+					</p>
+				</div>
 			</div>
 		</div>
 	);
