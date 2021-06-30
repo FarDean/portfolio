5a14635a3e4e81392982b09607de2d6bf77480bc
FarDean <64462409+FarDean@users.noreply.github.com>
2021-06-30 11:52:06 +0430

    about contnet add

--- src/comps/About.tsx
+++ src/comps/About.tsx
@@ -1,3 +1,17 @@
+import portrait from "././../assets/portrait.jpg";
+
 export const About: React.FC = (): JSX.Element => {
-	return <div>About Me</div>;
+	return (
+		<div>
+			<div>
+				<img src={portrait} alt="Face" />
+			</div>
+			<div>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi repudiandae modi
+				sequi rem eos ipsam mollitia quia. Fuga quae nemo quas laboriosam inventore minima
+				minus natus optio dignissimos autem dolores quibusdam quis blanditiis impedit,
+				distinctio est ad totam debitis voluptates! Deleniti facilis praesentium delectus?
+			</div>
+		</div>
+	);
 };
