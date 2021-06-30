25da13bdb8f862421032a4c58691ba70d01f845d
FarDean <64462409+FarDean@users.noreply.github.com>
2021-06-30 10:55:07 +0430

    added lazy loading

--- src/comps/Works.tsx
+++ src/comps/Works.tsx
@@ -15,7 +15,7 @@ export const Works = (): JSX.Element => {
 					<img src={blog} loading="lazy" alt="MERN Blog" />
 				</div>
 				<div className={styles.div2}>
-					<img src={football} alt="Football App" />
+					<img src={football} loading="lazy" alt="Football App" />
 				</div>
 				{/* <div className={styles.div3}> project3</div> */}
 			</div>
